c287a7d27f8f228ed162ad241617ac409c9a3452
HA <ha@hataeseon-ui-MacBookAir.local>
2023-01-05 21:46:24 +0900

    2023/1/5

--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -7,12 +7,54 @@
 
 import UIKit
 
-class FeedController: UIViewController {
-    
+private let reuseIdentifier = "Cell" //식별자
+
+class FeedController: UICollectionViewController {
+
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-
+        configureUI()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - Helpers
+    func configureUI(){
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+    //MARK: - UICollectionViewDataSource
+
+extension FeedController{
+    
+    //섹션 항목수
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    //컬렉션셀 보기 셀정의
+    //아직 컬렉션 셀에 등록을 안했음. configureUI에서 register해준다.
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
+        return cell
+    }
+}
+
+    //MARK: - UICollectionViewDelegateFlowLayout
+
+extension FeedController:UICollectionViewDelegateFlowLayout{
+    //프레임의 너비와 높이 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = view.frame.width
+        var height = width + 8 + 40 + 8
+        height += 50
+        height += 60
+        
+        return CGSize(width: width, height: height)
     }
 }
--- InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -21,14 +21,32 @@ class MainTabController: UITabBarController {
         view.backgroundColor = .white
         tabBar.backgroundColor = .white
         
-        let feed = FeedController()
-        let search = SearchController()
-        let imageSelector = ImageSelectorController()
-        let notifications = NotificationController()
-        let profile = ProfileController()
+        let layout = UICollectionViewFlowLayout()
+        
+        let feed = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "home_unselected"), selectedImage: UIImage(imageLiteralResourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
+        
+        let search = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "search_selected"), selectedImage: UIImage(imageLiteralResourceName: "search_unselected"), rootViewController: SearchController())
+        
+        let imageSelector = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "plus_unselected"), selectedImage: UIImage(imageLiteralResourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+        
+        let notifications = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "like_selected"), selectedImage: UIImage(imageLiteralResourceName: "like_unselected"), rootViewController: NotificationController())
+        
+        let profile = templateNavigationController(unselectedImage: UIImage(imageLiteralResourceName: "profile_selected"), selectedImage: UIImage(imageLiteralResourceName: "profile_unselected"), rootViewController: ProfileController())
         
         viewControllers = [feed, search, imageSelector, notifications, profile]
         
+        tabBar.tintColor = .black
+    }
+    
+    // 이미지를 네비게이션 컨트롤러 내부에 포함하는 뷰 컨트롤러
+    func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage,rootViewController:UIViewController) -> UINavigationController{
+        
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        
+        return nav
     }
     
 }
